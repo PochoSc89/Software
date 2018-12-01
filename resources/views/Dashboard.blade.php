28412989e79cc379f64536054d0c9393d4561637
PochoSc89 <nsantivanez.innovartech@gmail.com>
2018-12-01 16:37:47 -0300

    update

---
+++ resources/views/Dashboard.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Dashboard</title>
+  </head>
+  <body>
+    <h1>Dashboard</h1>
+    <header>
+      <nav>
+        <a href="<?php echo route('Soporte') ?>">Asistencia</a><br>
+        <a href="<?php echo route('Producto') ?>">Producto</a><br>
+        <a href="<?php echo route('Informacion')?>">Informes</a><br>
+        <a href="<?php echo route('PanelControl') ?>">Usuarios</a><br>
+      </nav>
+    </header>
+  </body>
+</html>
