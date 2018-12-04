15193111d986686348b929f6f0ec1bb09e7fe1ae
PochoSc89 <nsantivanez.innovartech@gmail.com>
2018-12-04 16:41:41 -0300

    ff

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 Route::get('/',['as'=>'Dashboard','uses'=> 'PagesController@Dashboard']);
 
 Route::get('Soporte', ['as'=>'Soporte','uses'=>'PagesController@Soporte']);
+
 Route::get('Stock', ['as'=>'Producto', 'uses'=> 'PagesController@Stock']);
+
 Route::get('Informacion', ['as'=>'Informacion', 'uses'=>'PagesController@Informes']);
+
 Route::get('Users', ['as'=>'PanelControl','uses'=>'PagesController@PanelControl']);
